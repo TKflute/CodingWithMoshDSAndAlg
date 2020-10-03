eae0ccb095478d82b1981ea8b30059be9c266082
Tara Kelly <kellyt2@students.westerntc.edu>
2020-10-03 10:26:23 -0500

    adding code from old LinkedList exercise

---
+++ CWMDataStructuresAndAlgorithms/src/www/codingwithmosh/dsaa/linkedlist/LinkedListDemo.java
@@ -0,0 +1,126 @@
+package www.codingwithmosh.dsaa.linkedlist;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class LinkedListDemo {
+
+	private SinglyLinkedList list;
+	
+	public static void main(String[] args) {
+		
+		LinkedListDemo demo = new LinkedListDemo();
+		demo.createList();
+		//demo.showList();
+		demo.addToStart();
+		demo.showList();
+		//System.out.println(demo.get(2).getData());
+		//System.out.println(demo.onList());
+		ListNode[] nodeArray = new ListNode[4];
+		nodeArray = demo.toArray(nodeArray);
+		for(ListNode node : nodeArray) {
+			System.out.println(node.getData());
+		}
+		//demo.clear();
+		//demo.remove();
+		//demo.showList();
+		//System.out.println(demo.length());
+		System.out.println(demo.indexOf("Patrick"));
+		demo.add();
+		demo.showList();
+		System.out.println(demo.lastIndexOf("Patrick"));
+		demo.set(4, "Bill");
+		demo.showList();
+		demo.add(3, "David");
+		demo.showList();
+	}
+		
+	public void createList() {
+		
+		list = new SinglyLinkedList();
+		
+		list.add("Spongebob");
+		list.add("Patrick");
+		list.add("Sandy");
+	}
+	
+	public void add() {
+		list.add("Patrick");
+	}
+	
+	public void add(int index, String data) {
+		list.add(index, data);
+	}
+	
+	public void showList() {
+		list.showList();
+	}
+	
+	public void clear() {
+		list.clear();
+	}
+	
+	public ListNode get(int index) {
+		return list.get(index);
+	}
+	
+	public String set(int index, String data) {
+		return list.set(index, data);
+	}
+	
+	public int indexOf(String data) {
+		return list.indexOf(data);
+	}
+	
+	public int lastIndexOf(String data) {
+		return list.lastIndexOf(data);
+	}
+	
+	public void remove() {
+		System.out.println("Remove test");
+		
+		while(!list.isEmpty()) {
+			//remove head
+			String nData = list.remove();
+			System.out.println(nData);
+		}
+	}
+	
+	public int length() {
+		return list.size();
+	}
+	
+	
+	public boolean onList() {
+		
+		if(!list.contains("Spongebob")) {
+			System.out.println("Error: Spongebob not found");
+			return false;
+		}
+		
+		if(!list.contains("Patrick")) {
+			System.out.println("Error: Patrick not found");
+			return false;
+		}
+		
+		if(!list.contains("Sandy")) {
+			System.out.println("Error: Sandy not found");
+			return false;
+		}
+		
+		if(list.contains("Larry")) {
+			System.out.println("Error: Larry should not be found");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public void addToStart() {
+		list.addToStart("Susie");
+	}
+	
+	public ListNode[] toArray(ListNode[] nodeArray) {
+		return list.toArray(nodeArray);
+	}
+}
---
+++ CWMDataStructuresAndAlgorithms/src/www/codingwithmosh/dsaa/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,248 @@
+package www.codingwithmosh.dsaa.linkedlist;
+
+public class SinglyLinkedList {
+
+	private ListNode head;
+	
+	public SinglyLinkedList() {
+		head = null;
+	}
+	
+	/*
+	public void add(T o) {
+		
+		if(head == null) {
+			head = new ListNode<T>(o, null);
+			return;
+		}
+		
+		ListNode cNode = head;
+		//get to last node in list
+		while(cNode.getNextNode() != null) {
+			cNode = cNode.c();
+		}
+		
+		//then add node as tail
+		cNode.setNextNode(new ListNode<T>(o, null));
+	}
+	
+	
+	public void addToStart(T o) {
+		
+		if(head == null) {
+			head = new ListNode<T>(o, null);
+			return;
+		}
+		
+		//get current head
+		ListNode oldHead = head;
+		ListNode newHead = new ListNode<T>(o, oldHead);
+		head = newHead;
+	}
+	
+	public boolean remove(T o) {
+		
+		if(!isEmpty()) {
+			ListNode cNode = head;
+			while(cNode.nextNode != null) {
+				if(cNode.data.equals(o)) {
+					ListNode<T> nodeToRemove = cNode;
+					cNode = nodeToRemove.nextNode;
+					return true;
+				}
+			}
+			return false;
+		}
+		
+	}*/
+	
+	
+	
+	
+	
+	
+	//will add node to list
+	//remember Lists are FIFO
+	public void add(String data) {
+		//if head is null, this is first item on list	
+		//make data the head
+		if(head == null) {
+			head = new ListNode(data, null);
+			return;
+		}
+		//find last node (tail)
+		ListNode cNode = head;
+		while(cNode.getNextNode() != null) {
+			cNode = cNode.getNextNode();
+		}
+		
+		//add new node as tail
+		cNode.setNextNode(new ListNode(data, null));
+	}
+	
+	public void addToStart(String data) {
+		
+		//make data the head if null
+		if(head == null) {
+			head = new ListNode(data, null);
+			return;
+		}
+		
+		ListNode oldHead = head;
+		ListNode newNode = new ListNode(data, oldHead);
+		head = newNode;
+	}
+	
+	public void add(int index, String data) {
+		//get node at index to add, reassign a new node at that index
+		ListNode nodeToMove = this.get(index - 1);
+		ListNode newNode = new ListNode(data, nodeToMove.getNextNode());
+		nodeToMove.setNextNode(newNode);
+	}
+	
+	//delete node at front of list
+	//return null if list is empty
+	
+	public String remove() {
+		//TODO: change this to return boolean
+		if(head == null) {
+			return null;
+		}
+		
+		ListNode oldHead = head;
+		head = oldHead.getNextNode();
+		
+		return oldHead.getData();
+	}
+	
+	public boolean remove(String data) {
+		
+		if(this.contains(data)) {
+			ListNode cNode = head;
+			while(cNode != null) {
+				if(cNode.getData().equals(data)) {
+					ListNode nodeToRemove = cNode;
+					cNode = nodeToRemove.getNextNode();
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
+	
+	public String remove(int index) {
+		
+		if(index >= this.size()) {
+			System.out.println("TODO: ERROR in remove");
+			return null;
+		}
+		
+		ListNode nodeToRemove = this.get(index);
+		this.remove(nodeToRemove.getData());
+		return nodeToRemove.getData();
+	}
+	
+	public void clear() {
+		head = null;
+	}
+	
+	//display all the data in the list
+	public void showList() {
+		ListNode cNode = head;
+		while(cNode != null) {
+			System.out.print(cNode.getData() + "-->");
+			cNode = cNode.getNextNode();
+		}
+			
+		System.out.println("NULL");
+	}
+	
+	//return # of nodes in list
+	public int size() {
+		
+		int count = 0;
+		ListNode cNode = head;
+		while(cNode != null) {
+			count++;
+			cNode = cNode.getNextNode();
+		}
+		
+		return count; 
+	}
+	
+	//returns whether item is in list or not
+	public boolean contains(String data) {
+		
+		ListNode cNode = head;
+		while(cNode != null) {
+			if(cNode.getData().equals(data)) {
+				return true;
+			}
+			
+			cNode = cNode.getNextNode();
+		}
+		
+		return false;
+	}
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	public ListNode get(int index) {
+		
+		ListNode cNode = head;
+		for(int i = 0; i < this.size(); i++) {
+			if(i == index) {
+				return cNode;
+			}
+			cNode = cNode.getNextNode();	
+		}
+		return null;
+	}
+	
+	public String set(int index, String data) {
+		
+		ListNode nodeToSet = this.get(index);
+		nodeToSet.setData(data);
+		return nodeToSet.getData();
+	}
+	
+	public int indexOf(String data) {
+		//TODO precondition if head is null - use contains()
+		ListNode cNode = head;
+		for(int i = 0; i < this.size(); i++) {
+			if(cNode.getData().contentEquals(data)) {
+				return i;
+			}
+			cNode = cNode.getNextNode();	
+		}
+		return -1;
+	}
+	
+	public int lastIndexOf(String data) {
+		//TODO precondition if head is null - use contains() which should use isEmpty		
+		ListNode cNode = head;
+		int lastIndex = 0;
+		for(int i = 0; i < this.size(); i++) {
+			if(cNode.getData().contentEquals(data)) {
+				lastIndex = i;
+			}
+			cNode = cNode.getNextNode();	
+		}
+		return lastIndex;	
+	}
+	
+	public ListNode[] toArray(ListNode[] nodeArray) {
+		if(this.size() != nodeArray.length) {
+			//TO DO: Handle exception
+			System.out.println("Error in toArray");
+			return null;
+		}
+		for(int i = 0; i < this.size(); i++) {
+			nodeArray[i] = this.get(i);
+		}
+		return nodeArray;	
+	}
+}
